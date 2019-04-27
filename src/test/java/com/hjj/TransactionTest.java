c75844838682259c965337b62127b5642e593f7c
houjianjun <749899757@qq.com>
2019-04-27 12:47:52 +0800

    spring tx

---
+++ src/test/java/com/hjj/TransactionTest.java
@@ -0,0 +1,40 @@
+package com.hjj;
+
+import com.hjj.service.AccountService;
+import com.hjj.service.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @Auther: HJJ
+ * @Date: 2019/04/24 09:02
+ * @Description:
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:META-INF/applicationContext.xml")
+public class TransactionTest {
+
+    @Autowired
+    private AccountService accountService;
+    @Autowired
+    private UserService userService;
+
+    @Autowired HelloWorld he;
+
+    @Test
+    public void tranTest(){
+        //遇到异常则回滚
+        accountService.transfer(1,2,100d);
+    }
+
+    @Test
+    public void test(){
+//        HelloWorld he = new  HelloWorld();
+//        he.setName("haha");
+//        he.hello();
+        userService.save();
+    }
+}
